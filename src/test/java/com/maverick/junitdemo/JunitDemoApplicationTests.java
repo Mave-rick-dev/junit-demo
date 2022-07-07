a6ced256782ad4977b66ec505532fe818d1df2b0
Om Narayan Singh <omnarayan.codehimalaya@gmail.com>
2022-07-07 17:45:33 +0545

    configured assertEquals and assertFalse

---
+++ src/test/java/com/maverick/junitdemo/JunitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maverick.junitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
